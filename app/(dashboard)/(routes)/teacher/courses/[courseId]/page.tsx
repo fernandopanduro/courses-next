f444e45cbf80767c31c8b63a641bf8e8a8e94967
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-10-01 15:07:26 -0700

    ✨ feat: Course Category Form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import CategoryForm from "./_components/category-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
 import TitleForm from "./_components/title-form";
@@ -22,6 +23,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   if (!course) {
     return redirect("/");
   }
+
+  const categories = await db.category.findMany({
+    orderBy: { name: "asc" },
+  });
+
+  console.log(categories);
 
   const requiereFields = [
     course.title,
@@ -55,6 +62,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <TitleForm initialData={course} courseId={course.id} />
           <DescriptionForm initialData={course} courseId={course.id} />
           <ImageForm initialData={course} courseId={course.id} />
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map(category => ({
+              label: category.name,
+              value: category.id,
+            }))}
+          />
         </div>
       </div>
     </div>
