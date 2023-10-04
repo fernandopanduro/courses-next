b6d73e19ab5663851b616882e45bd9fb6403a92a
Fernando Panduro <fernandogp2003pro@gmail.com>
2023-10-04 11:46:10 -0700

    ✨ feat: Courses Data Table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,26 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
 
 type Props = {};
 
-const CoursesPage = (props: Props) => {
+const CoursesPage = async (props: Props) => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/");
+
+  const courses = await db.course.findMany({
+    where: { userId },
+    orderBy: {
+      createdAt: "asc",
+    },
+  });
+
   return (
-    <div className="p">
-      <Link href={`/teacher/create`}>
-        <Button>New Course</Button>
-      </Link>
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
